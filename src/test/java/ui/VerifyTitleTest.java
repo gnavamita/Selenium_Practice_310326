b93504a8fa536bc822e7556b0706ce8ca1f11e2b
gnavamita <gnavamita@gmail.com>
2026-04-05 19:22:00 +0530

    adding new files

---
+++ src/test/java/ui/VerifyTitleTest.java
@@ -0,0 +1,20 @@
+package ui;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class VerifyTitleTest {
+
+@Test
+    public void TitleTest(){
+String ExpectedTitle = "Electronics, Cars, Fashion, Collectibles & More | eBay";
+        WebDriverManager.chromedriver().setup();
+        ChromeDriver driver = new ChromeDriver();
+        driver.get("https://www.ebay.com/");
+        String actualTitle = driver.getTitle();
+        Assert.assertEquals(actualTitle,ExpectedTitle);
+
+    }
+}
